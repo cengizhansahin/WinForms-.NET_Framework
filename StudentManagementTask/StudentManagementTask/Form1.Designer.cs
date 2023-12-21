74444c2923a941b58d011c35b6f38214bdc13db7
Cengizhan Şahin <cengizhan7sahin@gmail.com>
2023-12-21 17:32:41 +0300

    öğrenci listeleme, ekleme, güncelleme ve silme metodları başarılı şekilde oluşturuldu

--- StudentManagementTask/StudentManagementTask/Form1.Designer.cs
+++ StudentManagementTask/StudentManagementTask/Form1.Designer.cs
@@ -28,13 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_lastName = new System.Windows.Forms.TextBox();
+            this.text_firstName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(462, 288);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmbGender);
+            this.panel1.Controls.Add(this.btnSil);
+            this.panel1.Controls.Add(this.btnGuncelle);
+            this.panel1.Controls.Add(this.btnKaydet);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.text_lastName);
+            this.panel1.Controls.Add(this.text_firstName);
+            this.panel1.Location = new System.Drawing.Point(522, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 306);
+            this.panel1.TabIndex = 1;
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "ERKEK",
+            "KADIN"});
+            this.cmbGender.Location = new System.Drawing.Point(12, 166);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(121, 21);
+            this.cmbGender.TabIndex = 9;
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Red;
+            this.btnSil.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(12, 258);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(86, 23);
+            this.btnSil.TabIndex = 8;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.Cyan;
+            this.btnGuncelle.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(12, 228);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(86, 23);
+            this.btnGuncelle.TabIndex = 7;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.Lime;
+            this.btnKaydet.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(12, 198);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(86, 23);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "KAYDET";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(9, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 14);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Cinsiyet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(9, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Doğum Tarihi";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 117);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(136, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Öğrenci Soyadı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Öğrenci Adı";
+            // 
+            // text_lastName
+            // 
+            this.text_lastName.Location = new System.Drawing.Point(12, 75);
+            this.text_lastName.Name = "text_lastName";
+            this.text_lastName.Size = new System.Drawing.Size(100, 20);
+            this.text_lastName.TabIndex = 1;
+            // 
+            // text_firstName
+            // 
+            this.text_firstName.Location = new System.Drawing.Point(12, 32);
+            this.text_firstName.Name = "text_firstName";
+            this.text_firstName.Size = new System.Drawing.Size(100, 20);
+            this.text_firstName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_lastName;
+        private System.Windows.Forms.TextBox text_firstName;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- StudentManagementTask/StudentManagementTask/Form1.cs
+++ StudentManagementTask/StudentManagementTask/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,102 @@ namespace StudentManagementTask
 {
     public partial class Form1 : Form
     {
+        public static string connectionString = "Server=DESKTOP-S5SNUUO;Database=DB_STUDENT_MANAGEMENT;Trusted_Connection=True;";
+        SqlConnection SqlConnection = new SqlConnection(connectionString);
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+        }
+        public void RefreshData()
+        {
+            string query = "SELECT * FROM Table_Students";
+            SqlCommand cmd = new SqlCommand(query, SqlConnection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].ReadOnly = true;
+            }
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO Table_Students (FirstName,LastName,BirthDate,Gender) VALUES (@FirstName,@LastName,@BirthDate,@Gender)", SqlConnection);
+            sqlCommand.Parameters.AddWithValue("@FirstName", text_firstName.Text);
+            sqlCommand.Parameters.AddWithValue("@LastName", text_lastName.Text);
+            sqlCommand.Parameters.AddWithValue("@BirthDate", dateTimePicker1.Value);
+            sqlCommand.Parameters.AddWithValue("@Gender", cmbGender.SelectedItem.ToString());
+
+            try
+            {
+                SqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                SqlConnection.Close();
+                RefreshData();
+                MessageBox.Show("Öğrenci ekleme işlemi başarılı.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu!", ex.Message);
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                text_firstName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                text_lastName.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                if (dataGridView1.CurrentRow.Cells[3].Value != null && dataGridView1.CurrentRow.Cells[3].Value != DBNull.Value)
+                {
+                    dateTimePicker1.Value = Convert.ToDateTime(dataGridView1.CurrentRow.Cells[3].Value);
+                }
+                cmbGender.SelectedItem = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand("UPDATE Table_Students SET FirstName = @FirstName, LastName = @LastName , BirthDate = @BirthDate , Gender = @Gender WHERE StudentID = @StudentID", SqlConnection);
+            sqlCommand.Parameters.AddWithValue("@StudentID", Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value));
+            sqlCommand.Parameters.AddWithValue("@FirstName", text_firstName.Text);
+            sqlCommand.Parameters.AddWithValue("@LastName", text_lastName.Text);
+            sqlCommand.Parameters.AddWithValue("@BirthDate", dateTimePicker1.Value);
+            sqlCommand.Parameters.AddWithValue("@Gender", cmbGender.SelectedItem.ToString());
+            try
+            {
+                SqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                SqlConnection.Close();
+                RefreshData();
+                MessageBox.Show("Öğrenci güncelleme işlemi başarılı.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu!", ex.Message);
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand("DELETE FROM Table_Students WHERE StudentID=@StudentID", SqlConnection);
+            sqlCommand.Parameters.AddWithValue("@StudentID", Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value));
+
+            try
+            {
+                SqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                SqlConnection.Close();
+                RefreshData();
+                MessageBox.Show("Öğrenci silme işlemi başarılı.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu!", ex.Message);
+            }
         }
     }
 }
